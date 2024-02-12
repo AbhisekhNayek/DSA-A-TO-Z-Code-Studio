18f4e92b44cb0f3fde455ab47baf8543c94cf4e9
Abhisekh Nayek <115914826+AbhisekhNayek@users.noreply.github.com>
2024-02-12 19:31:53 +0530

    Create Ninja And The Star Pattern I.cpp

---
+++ Ninja And The Star Pattern I.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void PrintPattern(int n)
+{
+     // outer loop for no. of rows.
+     for(int i=0;i<n;i++){
+         
+         // inner loop for printing the stars at borders only.
+         for(int j=0;j<n;j++){
+             
+             if(i==0 || j==0 || i==n-1 || j==n-1)
+                cout<<"*";
+                
+             // if not border index, print space.
+             else cout<<" ";
+         }
+         
+          // As soon as the stars for each iteration are printed, we move to the
+          // next row and give a line break otherwise all stars
+          // would get printed in 1 line.
+          cout<<endl;
+     }
+      
+}
+
+int main()
+{   
+    int N;
+    cout<<"ENTER THE LAST RANGE :";
+    cin>>N;
+    
+    PrintPattern(N);
+
+    return 0;
+}
