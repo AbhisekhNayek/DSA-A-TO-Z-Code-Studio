16facec2b514fd0723169cef8c08fef8dce32e50
Abhisekh Nayek <115914826+AbhisekhNayek@users.noreply.github.com>
2024-02-12 19:01:50 +0530

    Create Alpha Triangle.cpp

---
+++ Alpha Triangle.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void PrintPattern(int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for (char ch='E'-i;ch<='E';ch++)
+        {
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    int num,t;
+    cout<<" Enter The Last Range : ";
+    cin>>num;
+    cin>>t;
+    for(int i=0;i<num;i++)
+    {
+        PrintPattern(t);
+    }
+    return 0;
+}
