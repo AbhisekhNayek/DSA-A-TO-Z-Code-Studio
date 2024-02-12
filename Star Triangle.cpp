a8bd7aab5396e96701b537b579fc0d8282053bf3
Abhisekh Nayek <115914826+AbhisekhNayek@users.noreply.github.com>
2024-02-12 12:08:21 +0530

    Create Star Triangle.cpp

---
+++ Star Triangle.cpp
@@ -0,0 +1,22 @@
+void nStarTriangle(int n) 
+{
+    for(int i=0;i<n;i++)
+    {
+    // Space 
+        for(int j=1;j<=n-i-1;j++)
+        {
+            cout<<" ";
+        }
+    //Star
+        for(int j=0;j<2*i+1;j++)
+        {
+            cout<<"*";
+        }
+    //Space
+        for(int j=0;j<n-i-1;j++)
+        {
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
