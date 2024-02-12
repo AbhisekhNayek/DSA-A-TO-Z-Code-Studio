3fe3a71f36d9282ba1d4851392b403e7506d5eda
Abhisekh Nayek <115914826+AbhisekhNayek@users.noreply.github.com>
2024-02-12 18:23:09 +0530

    Create  Increasing Number Triangle.cpp

---
+++ Increasing Number Triangle.cpp
@@ -0,0 +1,13 @@
+void nNumberTriangle(int n) 
+{
+    int num=1;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<num<<" ";
+            num=num+1;
+        }
+        cout<<endl;
+    }
+}
