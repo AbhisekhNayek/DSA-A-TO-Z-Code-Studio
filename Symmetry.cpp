e6d12f18b8e3d81d7fe3ddb00d8aca38f2fbe3d6
Abhisekh Nayek <115914826+AbhisekhNayek@users.noreply.github.com>
2024-02-12 19:26:38 +0530

    Create Symmetry.cpp

---
+++ Symmetry.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void PrintPattern(int n)
+{
+      // initialising the spaces.
+      int spaces = 2*n-2;
+      
+      // Outer loop for printing row.
+      for(int i = 1;i<=2*n-1;i++){
+          
+          // stars for first half
+          int stars = i;
+          
+          // stars for the second half.
+          if(i>n) stars = 2*n - i;
+          
+          //for printing the stars
+          for(int j=1;j<=stars;j++){
+              cout<<"*";
+          }
+          
+          //for printing the spaces
+          for(int j = 1;j<=spaces;j++){
+              cout<<" ";
+          }
+          
+          //for printing the stars
+          for(int j = 1;j<=stars;j++){
+              cout<<"*";
+          }
+          
+          // As soon as the stars for each iteration are printed, we move to the
+          // next row and give a line break otherwise all stars
+          // would get printed in 1 line.
+          cout<<endl;
+          if(i<n) spaces -=2;
+          else spaces +=2;
+      }
+      
+}
+
+int main()
+{   
+    int N;
+    cout<<"ENTER THE LAST RANGE :";
+    cin>>N;
+    
+    PrintPattern(N);
+
+    return 0;
+}
